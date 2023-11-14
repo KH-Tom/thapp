804cb69e4aee23c8072b429bf829212439ce3e39
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-11-13 23:52:33 -0700

    lab3

--- ToDoList.jsx
+++ ToDoList.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import {ScrollView, View, Text, Pressable} from 'react-native';
-import styles from './your-styles-file'; // Update the path to your styles file
+import styles from './your-styles-file';
 
-function ToDoList() {
+function ToDoList({tasks}) {
   return (
     <ScrollView>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Do laundry</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task]}>
-          <Text style={styles.taskText}>Go to gym</Text>
-        </View>
-      </Pressable>
-      <Pressable>
-        <View style={[styles.task, styles.completed]}>
-          <Text style={styles.taskText}>Walk dog</Text>
-        </View>
-      </Pressable>
+      {tasks.map((task, index) => (
+        <Pressable key={index}>
+          <View style={[styles.task]}>
+            <Text style={styles.taskText}>{task}</Text>
+          </View>
+        </Pressable>
+      ))}
     </ScrollView>
   );
 }
