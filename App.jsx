d6368844a209ed7146ecd65720a8edd0d168b859
KH-Tom <116770365+KH-Tom@users.noreply.github.com>
2023-11-13 23:20:39 -0700

    33
    33

--- App.jsx
+++ App.jsx
@@ -72,8 +72,8 @@ function App() {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App122.jsx</Text> to change this
-            screen and then come back to see your edits.
+            Edit <Text style={styles.highlight}>App1234.jsx</Text> to change
+            this screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
